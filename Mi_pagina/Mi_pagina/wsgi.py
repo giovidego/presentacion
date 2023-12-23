25801bf1eac26cf49a7991dfc54b903d49264fea
giovidego <120355617+giovidego@users.noreply.github.com>
2023-12-23 15:34:16 -0300

    Update wsgi.py

--- Mi_pagina/Mi_pagina/wsgi.py
+++ Mi_pagina/Mi_pagina/wsgi.py
@@ -14,4 +14,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mi_pagina.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root = os.path.join(BASE_DIR, '/staticfiles')
+application = WhiteNoise(application, root = os.path.join(BASE_DIR, '/staticfiles'))
