70d878b3b9805c6fd5ea6e125b5fc79856a41f9b
giovidego <120355617+giovidego@users.noreply.github.com>
2023-12-23 13:11:54 -0300

    Update settings.py

--- Mi_pagina/Mi_pagina/settings.py
+++ Mi_pagina/Mi_pagina/settings.py
@@ -121,22 +121,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-#STATIC_URL = 'static/'
-#STATICFILES_DIRS = [BASE_DIR/'staticfiles',BASE_DIR/'static']
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'staticfiles',BASE_DIR/'static']
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 # Following settings only make sense on production and may break development environments.
-if not DEBUG:
+#if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
