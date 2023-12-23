49ed06b1dda276b1258d66b4379a232efa9e0b00
giovidego <120355617+giovidego@users.noreply.github.com>
2023-12-23 15:37:06 -0300

    Update wsgi.py

--- Mi_pagina/Mi_pagina/wsgi.py
+++ Mi_pagina/Mi_pagina/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+BASE_DIR = Path(__file__).resolve().parent.parent
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mi_pagina.settings')
 
 application = get_wsgi_application()
