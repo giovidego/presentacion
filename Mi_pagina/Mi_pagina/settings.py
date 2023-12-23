9391bcb331d7e18e8b3b0ded7ade63583bf8f08c
giovidego <120355617+giovidego@users.noreply.github.com>
2023-12-23 11:20:56 -0300

    Update settings.py

--- Mi_pagina/Mi_pagina/settings.py
+++ Mi_pagina/Mi_pagina/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/'static',]
+#STATICFILES_DIRS = [BASE_DIR/'static',]
 
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
