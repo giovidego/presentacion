870c61b51788c2a321d42c2ed0af5edb5c87b235
giovidego <120355617+giovidego@users.noreply.github.com>
2023-12-23 11:53:21 -0300

    Update settings.py

--- Mi_pagina/Mi_pagina/settings.py
+++ Mi_pagina/Mi_pagina/settings.py
@@ -35,7 +35,6 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
