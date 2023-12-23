591d6fa62d77c12cfef7e2a312f46c7cbd7eb689
giovidego <120355617+giovidego@users.noreply.github.com>
2023-12-23 11:34:17 -0300

    Update settings.py

--- Mi_pagina/Mi_pagina/settings.py
+++ Mi_pagina/Mi_pagina/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 #STATIC_URL = 'static/'
 #STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
